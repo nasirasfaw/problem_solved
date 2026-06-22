6c210020f47f0f40ad7bffb7570c4b3a4309070d
Nasir Asfaw <nasirasfawk@gmail.com>
2026-06-22 17:09:23 +0300

    1915A - Odd one our- to find the unique number among three

---
+++ 1915A.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for t in range(t):
+    a, b, c = map(int, input().split())
+    
+    if a == b:
+        print(c)
+    elif b == c:
+        print(a)
+    elif a == c:
+        print(b)
